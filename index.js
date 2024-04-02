e4076dad07cbf5813aa6a885a4ae90241907a45f
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 23:32:39 +0500

    v1.0.0

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 #! /usr/bin/env node
-export {};
+import inquirer from "inquirer";
+let todos = [];
+let repeat = true;
+while (repeat) {
+    let prompts = await inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What do you want to add to your todo list?",
+        },
+        {
+            type: "confirm",
+            name: "comfirmation",
+            message: "Do you want to add more items to your todo list?",
+            default: true,
+        }
+    ]);
+    todos.push(prompts.title);
+    repeat = prompts.comfirmation;
+}
+;
+console.table(todos);
--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
 #! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let todos = [];
+
+let repeat = true;
+
+while (repeat) {
+	let prompts = await inquirer.prompt(
+		[
+			{
+				type: "input",
+				name: "title",
+				message: "What do you want to add to your todo list?",
+			},
+			{
+				type: "confirm",
+				name: "comfirmation",
+				message: "Do you want to add more items to your todo list?",
+				default: true,
+			}
+		]
+	);
+	
+	todos.push(prompts.title);
+	
+   repeat = prompts.comfirmation;
+};
+
+console.table(todos);
